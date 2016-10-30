d9fa6a6064b86de3d9891343f069ad183fbd31d4
devstopfix <james@devstopfix.net>
2016-10-30 16:42:30 +0000

    Solarized

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject mimic.board "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject mimic.board "0.1.0"
+  :description "Mimic Board"
+  :url "https://github.com/devstopfix/mimic-board"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
 
   :min-lein-version "2.7.1"
 
